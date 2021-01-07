a9055b7bca0861d52ac600e9159b9cf1e1771a04
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-07 11:41:43 -0800

    style page and add make functionality to page

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-// import functions and grab DOM elements
-const welcomeElem = document.getElementById('welcome');
-const body = document.getElementById('wholePage');
-const myButton = document.getElementById('change-button');
-const otherButton = document.getElementById('fix-button');
-const input = document.getElementById('text');
+const sumBtn = document.getElementById('sum-button');
+const subBtn = document.getElementById('subtract-button');
+const multBtn = document.getElementById('multiply-button');
+const divBtn = document.getElementById('divide-button');
+const mathResults = document.getElementById('answer');
 
-// initialize state
 
-// set event listeners to update state and DOM
-
-myButton.addEventListener('click', () => {
-    welcomeElem.style.color = 'red';
-    welcomeElem.textContent = input.value;
-    body.style.background = 'blue';
+sumBtn.addEventListener('click', () => {
+    const num1 = document.getElementById('number-1');
+    const num2 = document.getElementById('number-2');
+    const result = Number(num1.value) + Number(num2.value);
+    mathResults.textContent = 'Your Answer is ' + result + '!';
 });
 
-otherButton.addEventListener('click', () => {
-    location.reload(false);
+subBtn.addEventListener('click', () => {
+    const num1 = document.getElementById('number-1');
+    const num2 = document.getElementById('number-2');
+    const result = Number(num1.value) - Number(num2.value);
+    mathResults.textContent = 'Your Answer is ' + result + '!';
+});
+
+multBtn.addEventListener('click', () => {
+    const num1 = document.getElementById('number-1');
+    const num2 = document.getElementById('number-2');
+    const result = Number(num1.value) * Number(num2.value);
+    mathResults.textContent = 'Your Answer is ' + result + '!';
+});
+
+divBtn.addEventListener('click', () => {
+    const num1 = document.getElementById('number-1');
+    const num2 = document.getElementById('number-2');
+    const result = Number(num1.value) / Number(num2.value);
+    mathResults.textContent = 'Your Answer is ' + result + '!';
+    if (num1 = NaN) {
+        mathResults.textContent = 'You did not enter a NUMBER!';
+    }
 });
